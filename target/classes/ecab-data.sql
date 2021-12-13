cd2638275d553d7626465842f9fce67a3714ced6
dineshmoorthy <contactdineshmoorthy@gmail.com>
2021-12-13 19:01:15 +0530

    deleting target

--- target/classes/ecab-data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO EMPLOYEZ (id, empName, empId) VALUES (5, 'DAMMU', 'emp091');
-INSERT INTO EMPLOYEZ (id, empName, empId) VALUES (6, 'SOMMU', 'emp092');
--- target/classes/ecab-schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE EMPLOYEZ
-        (
-        id NUMBER(10) NOT NULL,
-        empName VARCHAR2(50) NOT NULL,
-		empId VARCHAR2(50) NOT NULL,
-        PRIMARY KEY(id)
-        );
